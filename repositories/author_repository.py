dfadeeb678aab43cad709e49e5790bddb44d5362
Sergio <sergio.eneide@protonmail.com>
2021-07-12 17:47:42 +0100

    16th commit - added author repository with pseudo code

---
+++ repositories/author_repository.py
@@ -0,0 +1,7 @@
+# from the database run_sql import self
+from db.run_sql import run_sql
+
+# from the models directory, import the Author class from the author.py file
+from models.author import Author
+# from the models directory, import the Tome class from the tome.py file
+from models.tome import Tome
