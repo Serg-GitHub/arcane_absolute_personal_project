e83d6496335b4b99f8dfbd90bace3f558f517b84
Sergio <sergio.eneide@protonmail.com>
2021-07-13 20:32:22 +0100

    commit 32 - added route

--- controllers/tomes_controller.py
+++ controllers/tomes_controller.py
@@ -10,3 +10,8 @@ tomes_blueprint = Blueprint("tomes", __name__)
 def tomes():
     tomes = tome_repository.select_all()
     return render_template("tomes/index.html", all_tomes = tomes)
+
+@tomes_blueprint.route("/tomes/new", methods=['GET'])
+def new_tome():
+    authors = author_repository.select_all()
+    return render_template("tomes/new.html", all_authors = authors)    
