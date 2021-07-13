2f6b030197d08d635a3e1248ac399e4b80a2cc52
Sergio <sergio.eneide@protonmail.com>
2021-07-13 10:02:24 +0100

    17th commit - started on console.py

--- console.py
+++ console.py
@@ -1,3 +1,8 @@
 import pdb
 from models.tome import Tome
 from models.author import Author
+
+import repositories.tome_repository as tome_repository
+import repositories.author_repository as author_repository
+
+
