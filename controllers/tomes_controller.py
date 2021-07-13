55c78dffc741d3fb9c6d098207f32b8b8cc4bd2d
Sergio <sergio.eneide@protonmail.com>
2021-07-13 19:43:30 +0100

    commit 31 - added nested templates

--- controllers/tomes_controller.py
+++ controllers/tomes_controller.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.tome import Tome
+import repositories.tome_repository as tome_repository
+import repositories.author_repository as author_repository
 
 tomes_blueprint = Blueprint("tomes", __name__)
+
+@tomes_blueprint.route("/tomes")
+def tomes():
+    tomes = tome_repository.select_all()
+    return render_template("tomes/index.html", all_tomes = tomes)
